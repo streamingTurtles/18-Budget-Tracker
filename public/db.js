9080b17f9553368df880c749299ca6da553593ad
Vivianne Cardillo <viviannecardillo@Viviannes-MBP.fios-router.home>
2021-03-23 16:42:25 -0400

    added code for db.js, updated server.js to prep for Heroku deployment, confirmed when offline that indexDB updates key/value fields when going backonline.

---
+++ public/db.js
@@ -0,0 +1,73 @@
+let db;
+// create a new "budget" database.
+const request = window.indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+   // create the object variable for out indexDB store named "pending" 
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+
+
+};
+
+request.onerror = function(event) {
+  console.log("argggg - something wrong - undefined! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    console.log('saveRecord');
+    
+  // give access to add read & write to the indexDB
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+
+  // add record to your store using the add method.
+  store.add(record);
+  console.log("added a record here");
+}
+
+function checkDatabase() {
+  // to open a transactions on your "pending" object store
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  console.log('getAll: ', getAll);
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        // if successful, open a transaction on your pending db
+        const transaction = db.transaction(["pending"], "readwrite");
+
+        // access the pending object store
+        const store = transaction.objectStore("pending");
+        console.log("about to clear here")
+        // clear all items in your store - remove so that I can see the data in the 
+        // browser in the indexDB section in chrome Dev Tools > application in the pending 
+        // if left in - its always clearing
+        // store.clear();
+      });
+    }
+  };
+}
+
+// event listner for the app when its back online
+window.addEventListener("online", checkDatabase);
--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// updatd for Heroku environmental variable or localhost option
+// for when either connected to Atlas or locally indexDB
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +18,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -24,5 +28,5 @@ mongoose.connect("mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App is running on port ${PORT}!`);
 });
